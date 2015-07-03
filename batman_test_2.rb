00d613d36dba2e34dc4e458386074a823654c8a4
Regis <boudinot.regis@yahoo.com>
2015-07-03 11:19:44 -0600

    rollin with the workflow

---
+++ batman_test_2.rb
@@ -0,0 +1,9 @@
+  # nil
+test_2 = "This is awesome"
+  # "This is awesome"
+whoa = "WHOAAAA"
+  # "WHOAAAA"
+ok = "I am done testing. Time to brainstorm"
+  # "I am done testing. Time to brainstorm"
+puts "#{test_2}! '#{whoa}'...#{ok}"
+  # nil
